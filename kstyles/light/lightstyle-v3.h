e4d562da25fed8187bac692157bc5dc771d973f0
Iegor <rmtdev@gmail.com>
2013-12-30 14:32:53 +0000

    reformat src

--- kstyles/light/lightstyle-v3.h
+++ kstyles/light/lightstyle-v3.h
@@ -39,51 +39,52 @@
 
 class Q_EXPORT_STYLE_LIGHT_V3 LightStyleV3 : public KStyle
 {
-    Q_OBJECT
-    QStyle *basestyle;
+	Q_OBJECT
+	QStyle *basestyle;
 
 public:
-    LightStyleV3();
-    virtual ~LightStyleV3();
+	LightStyleV3();
+	virtual ~LightStyleV3();
 
-    void polishPopupMenu( QPopupMenu * );
+	void polishPopupMenu(QPopupMenu *);
 
-    void drawPrimitive(PrimitiveElement, QPainter *, const QRect &, const QColorGroup &,
-		       SFlags = Style_Default,
-		       const QStyleOption & = QStyleOption::Default ) const;
+	void drawPrimitive(PrimitiveElement, QPainter *, const QRect &, const QColorGroup &,
+	                   SFlags = Style_Default,
+	                   const QStyleOption & = QStyleOption::Default) const;
 
-    void drawControl(ControlElement, QPainter *, const QWidget *, const QRect &,
-		     const QColorGroup &, SFlags = Style_Default,
-		     const QStyleOption & = QStyleOption::Default ) const;
-    void drawControlMask(ControlElement, QPainter *, const QWidget *, const QRect &,
-			 const QStyleOption & = QStyleOption::Default) const;
+	void drawControl(ControlElement, QPainter *, const QWidget *, const QRect &,
+	                 const QColorGroup &, SFlags = Style_Default,
+	                 const QStyleOption & = QStyleOption::Default) const;
+	void drawControlMask(ControlElement, QPainter *, const QWidget *, const QRect &,
+	                     const QStyleOption & = QStyleOption::Default) const;
 
-    QRect subRect(SubRect, const QWidget *) const;
+	QRect subRect(SubRect, const QWidget *) const;
 
-    void drawComplexControl(ComplexControl, QPainter *, const QWidget *, const QRect &,
-			    const QColorGroup &, SFlags = Style_Default,
-			    SCFlags = SC_All, SCFlags = SC_None,
-			    const QStyleOption & = QStyleOption::Default ) const;
+	void drawComplexControl(ComplexControl, QPainter *, const QWidget *, const QRect &,
+	                        const QColorGroup &, SFlags = Style_Default,
+	                        SCFlags = SC_All, SCFlags = SC_None,
+	                        const QStyleOption & = QStyleOption::Default) const;
 
-    QRect querySubControlMetrics(ComplexControl, const QWidget *, SubControl,
-				 const QStyleOption & = QStyleOption::Default ) const;
+	QRect querySubControlMetrics(ComplexControl, const QWidget *, SubControl,
+	                             const QStyleOption & = QStyleOption::Default) const;
 
-    SubControl querySubControl(ComplexControl, const QWidget *, const QPoint &,
-			       const QStyleOption &data = QStyleOption::Default ) const;
+	SubControl querySubControl(ComplexControl, const QWidget *, const QPoint &,
+	                           const QStyleOption &data = QStyleOption::Default) const;
 
-    int pixelMetric(PixelMetric, const QWidget * = 0 ) const;
+	int pixelMetric(PixelMetric, const QWidget * = 0) const;
 
-    QSize sizeFromContents(ContentsType, const QWidget *, const QSize &,
-			   const QStyleOption & = QStyleOption::Default ) const;
+	QSize sizeFromContents(ContentsType, const QWidget *, const QSize &,
+	                       const QStyleOption & = QStyleOption::Default) const;
 
-    int styleHint(StyleHint, const QWidget * = 0,
-		  const QStyleOption & = QStyleOption::Default,
-		  QStyleHintReturn * = 0 ) const;
+	int styleHint(StyleHint, const QWidget * = 0,
+	              const QStyleOption & = QStyleOption::Default,
+	              QStyleHintReturn * = 0) const;
 
-    QPixmap stylePixmap( StylePixmap stylepixmap,
-			 const QWidget* widget = 0,
-			 const QStyleOption& = QStyleOption::Default ) const;
+	QPixmap stylePixmap(StylePixmap stylepixmap,
+	                    const QWidget* widget = 0,
+	                    const QStyleOption& = QStyleOption::Default) const;
 };
 
 
 #endif // LIGHTSTYLE_V3_H
+//kate: space-indent on; tab-width 4; indent-width 4; indent-mode cstyle; encoding UTF-8;
