db160890ad3a6856b818c404ba8a26180707fc6e
Iegor <rmtdev@gmail.com>
2014-01-29 04:13:16 +0000

    added aurora style

---
+++ kstyles/aurora/aurora.cpp
@@ -0,0 +1,15 @@
+//
+// C++ Implementation: aurora
+//
+// Description:
+//
+//
+// Author: The package is maintained by Iegor <rmtdev@gmail.com>, (C) 2014
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+// #include <qimage.h>
+// #include <kpixmap.h>
+
+#include "aurora.h"
---
+++ kstyles/aurora/aurora.h
@@ -0,0 +1,27 @@
+//
+// C++ Interface: aurora
+//
+// Description:
+//
+//
+// Author: The package is maintained by Iegor <rmtdev@gmail.com>, (C) 2014
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef __AURORA_H
+#define __AURORA_H
+
+#include <kstyle.h>
+
+class AuroraStyle : public KStyle {
+private:
+    Q_OBJECT;
+public:
+    AuroraStyle() {
+    }
+    ~AuroraStyle() {
+    }
+};
+
+#endif //__AURORA_H
